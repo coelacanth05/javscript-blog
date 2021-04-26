2e9d82babfc9b5ca9bb72f73d044f77448b22cdb
kai <63350063+coelacanth05@users.noreply.github.com>
2021-04-27 06:19:13 +0900

    if文

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+if(window.confirm("明日は晴れますか？")){
+console.log('はい、晴れです');}
+else{
+console.log("明日は雨です");
+}
