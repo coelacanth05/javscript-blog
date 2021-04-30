04babb56307ac48597bd329cfa6addc0ae292076
kai <63350063+coelacanth05@users.noreply.github.com>
2021-05-01 05:51:50 +0900

    for

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-let c='B';
-switch(c){
-  case'A':
-  console.log('Aクラスです。');
-  break;
-  case'B':
-  console.log('Bクラスです。');
-  break;
-  case'C':
-  console.log('Cクラスです。');
-  break;
-  default:
-    console.log('クラス外です。');
-    break;
-  }
+for (let x=5; x<10; x++){
+  console.log('xの値は'+x);
+}
 
