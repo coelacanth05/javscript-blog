43d13804980938d0a6b50292506801eef459b00d
kai <63350063+coelacanth05@users.noreply.github.com>
2021-06-06 06:30:12 +0900

    2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// const win_width = window.innerWidth;
+// const btnC = document.getElementById("clickEvent") 
+// btnC.addEventListener("click" , event => {
+//   alert(win_width);
+// })
 
-const win_width = window.innerWidth;
-const btnC = document.getElementById("clickEvent") 
-btnC.addEventListener("click" , event => {
-  alert(win_width);
-})
+const num1 = 5
+const num2 = 8
+alert(num1+num2)
