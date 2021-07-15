2357684d4b9c24103623e81c0bc52894e023c8f0
kai <63350063+coelacanth05@users.noreply.github.com>
2021-07-15 20:29:47 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
-document.getElementById("btn").onclick = function() {
-setTimeout(function(){
-alert("2秒経過")
-},2000)
-}
+// document.getElementById("btn").onclick = function() {
+// setTimeout(function(){
+// alert("2秒経過")
+// },2000)
+// }
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function(){
+    window.setTimeout(function(){
+        alert('時間切れです');
+    }, 2000);
+});
