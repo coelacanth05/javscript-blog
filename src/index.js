f11eb3c64a463a93b5a2f7b5bda598dbf1d31db1
kai <63350063+coelacanth05@users.noreply.github.com>
2021-07-29 20:26:54 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-
-// document.getElementById("btn").onclick = function() {
-// setTimeout(function(){
-// alert("2秒経過")
-// },2000)
-// }
-let btn = document.getElementById('btn');
-btn.addEventListener('click', function(){
-    window.setTimeout(function(){
-        alert('時間切れです');
-    }, 2000);
-});
+// let fruits = ["リンゴ", "メロン", "バナナ"];
+alert("アラートのメッセージ");
