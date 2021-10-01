63a55bcbbf276799ec29d7b1909d1cf86f7e9272
kai <63350063+coelacanth05@users.noreply.github.com>
2021-10-01 20:38:53 +0900

    end en

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-$(function () {
-  $("p").removeClass("red");
-});
+// $("p").on('click',function(){
+//   $(this).toggleClass("is-active")
+// })
+
+// document.getElementById('p').style.color = "bule"
+
+document.getElementById("p").onclick = () => {
+  document.getElementById("p").classList.toggle("is-active");
+};
