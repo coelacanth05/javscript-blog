a4d4542bc377c143f75b3217f75af7d6e1fddfe1
kai <63350063+coelacanth05@users.noreply.github.com>
2021-10-31 09:56:39 +0900

    忍者14,15

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-// $(() => {
-//   $(".text").prepend("<p>日曜日</p>");
+// document.getElementById("txt").addEventListener("click", () => {
+//   document.getElementById("ci-txt").style.display = "block";
 // });
 
-// const pTop = document.getElementById('id-text')
-// const newP = document.createElement('p')
-// newP.textContent = '日曜日'
-// pTop.before(newP)
-
-// $(() => {
-//   $(".text").append("<p>日曜日</p>");
+// $(function () {
+//   $(".txt").click(function () {
+//     $(".ci-txt").show();
+//   });
 // });
 
-const pTop = document.getElementById("id-text");
-const newP = document.createElement("p");
-newP.textContent = "日曜日";
-pTop.after(newP);
+document.getElementById("txt").addEventListener("click", () => {
+  document.getElementById("ci-txt").style.display = "none";
+});
+
+// $(function () {
+//   $(".txt").click(function () {
+//     $(".ci-txt").hide();
+//   });
+// });
+
+// document.getElementById("txt").addEventListener("click", () => {
+//   const ciTxt = document.getElementById("ci-txt");
+//   if (ciTxt.style.display == "none") {
+//     ciTxt.style.display = "block";
+//   } else {
+//     ciTxt.style.display = "none";
+//   }
+// });
