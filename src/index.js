59f805d57f3dac901aff7edf84bef5ef864c30ae
kai <63350063+coelacanth05@users.noreply.github.com>
2021-12-22 21:07:58 +0900

    fadeIn

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
-// document.getElementById("txt").addEventListener("click", () => {
-//   document.getElementById("ci-txt").style.display = "block";
-// });
-
-// $(function () {
-//   $(".txt").click(function () {
-//     $(".ci-txt").show();
-//   });
-// });
-
-// document.getElementById("txt").addEventListener("click", () => {
-//   document.getElementById("ci-txt").style.display = "none";
-// });
-
-// $(function () {
-//   $(".txt").click(function () {
-//     $(".ci-txt").hide();
-//   });
-// });
-
 document.getElementById("txt").addEventListener("click", () => {
-  const ciTxt = document.getElementById("ci-txt");
-  if (ciTxt.style.display == "none") {
-    ciTxt.style.display = "block";
-  } else {
-    ciTxt.style.display = "none";
-  }
+  fadeIn(document.getElementById("fadeIn_txt", (.style.opactity = 1)));
 });
+// $(function () {
+//   $("div").fadeIn();
+// });
