1022c08673029a1603c7315c3af36b797485f3a3
kai <63350063+coelacanth05@users.noreply.github.com>
2021-12-23 20:54:26 +0900

    fadeIn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-document.getElementById("txt").addEventListener("click", () => {
-  fadeIn(document.getElementById("fadeIn_txt", (.style.opactity = 1)));
-});
-// $(function () {
-//   $("div").fadeIn();
+// document.getElementById("txt").addEventListener("click", () => {
+//   const uptxt = document.getElementById("fadeIn_txt");
+//   uptxt.style.display = "block";
 // });
+$(function () {
+  $(".txt").click(function () {
+    $(".fadeIn_txt").fadeIn("slow");
+  });
+});
