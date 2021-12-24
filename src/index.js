418a8a10082b770517ad51f6ce8d5572525bbc23
kai <63350063+coelacanth05@users.noreply.github.com>
2021-12-24 20:35:56 +0900

    end end

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@
 //   const uptxt = document.getElementById("fadeIn_txt");
 //   uptxt.style.display = "block";
 // });
+
+// $(function () {
+//   $(".txt").click(function () {
+//     $(".fadeIn_txt").fadeIn("slow");
+//   });
+// });
+
+// document.getElementById("txt").addEventListener("click", () => {
+//   const outtxt = document.getElementById("fadeOut_txt");
+//   outtxt.style.display = "none";
+// });
+
 $(function () {
   $(".txt").click(function () {
-    $(".fadeIn_txt").fadeIn("slow");
+    $(".fadeOut_txt").fadeOut("slow");
   });
 });
